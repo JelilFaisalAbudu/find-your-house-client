e6919dbacce1a95feb59a6c8175e1aacf5f80273
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-13 12:49:07 +0000

    Add getPublicContent, getUserBoard and getUserFavorites to user.services

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,1 +1,20 @@
 import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'localhost:3000/api/v1/';
+
+const getPublicContent = () => axios.get(`${API_URL}houses`);
+
+const getUserboard = () => axios.get(`${API_URL}user`, { headers: authHeader() });
+
+const getUserFavorites = userId => (
+  axios.get(`${API_URL}users/${userId}/favorites`, { headers: authHeader() })
+);
+
+const userServices = {
+  getPublicContent,
+  getUserboard,
+  getUserFavorites,
+};
+
+export default userServices;
