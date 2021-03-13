c5e8ef7735c0ef0ac0340c720c8c16dbb699d245
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-13 12:05:51 +0000

    Add login(), logout() and register() functions to the authServices

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,1 +1,32 @@
 import axios from 'axios';
+
+const API_URL = 'http:// localhost:8080/api/v1/';
+
+const login = (email, password) => axios
+  .post(`${API_URL}auth/login`, {
+    email,
+    password,
+  })
+  .then(response => {
+    if (response.data.auth_token) {
+      localStorage.setItem('user', JSON.stringify(response.data));
+    }
+    return response.data;
+  });
+
+const logout = () => localStorage.removeItem('user');
+
+const register = (name, email, password, password_confirmation) => axios.post(`${API_URL}signup`, {
+  name,
+  email,
+  password,
+  password_confirmation,
+});
+
+const authService = {
+  login,
+  register,
+  logout,
+};
+
+export default authService;
