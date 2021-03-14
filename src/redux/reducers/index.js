de40a69bd3077f64d1f0a3454db31f17f528703e
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-14 05:10:48 +0000

    Create a combine reducer

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import authReducer from './auth';
+import messageReducer from './message';
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  message: messageReducer,
+});
+
+export default rootReducer;
