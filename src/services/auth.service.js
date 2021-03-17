7f8b453b3541bfaf95edfe1dd877406a6832c8ed
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-17 10:50:20 +0000

    Update the auth.services and the  user.services files

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,28 +1,35 @@
 /* eslint-disable camelcase */
 import axios from 'axios';
 
-const API_URL = 'http:// localhost:3000/api/v1/';
+const API_URL = 'http://localhost:3001/api/v1/';
 
-const login = (email, password) => axios
-  .post(`${API_URL}auth/login`, {
-    email,
-    password,
-  })
-  .then(response => {
-    if (response.data.auth_token) {
-      localStorage.setItem('user', JSON.stringify(response.data.auth_token));
-    }
-    return response.data;
-  });
+const login = async (email, password) => {
+  const response = await axios
+    .post(`${API_URL}auth/login`, {
+      email,
+      password,
+    });
+  if (response.data.auth_token) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+  return response.data;
+};
 
 const logout = () => localStorage.removeItem('user');
 
-const register = (name, email, password, password_confirmation) => axios.post(`${API_URL}signup`, {
-  name,
-  email,
-  password,
-  password_confirmation,
-});
+const register = async (name, email, password, password_confirmation) => {
+  const response = await axios
+    .post(`${API_URL}/signup`, {
+      name,
+      email,
+      password,
+      password_confirmation,
+    });
+  if (response.data.auth_token) {
+    localStorage.setItem('user', JSON.stringify(response.data));
+  }
+  return response.data;
+};
 
 const authService = {
   login,
--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'localhost:3000/api/v1/';
+const API_URL = 'http://localhost:3001/api/v1/';
 
 const getPublicContent = () => axios.get(`${API_URL}houses`);
 
-const getUserboard = () => axios.get(`${API_URL}user`, { headers: authHeader() });
+const getUserboard = userId => (
+  axios.get(`${API_URL}users/${userId}/favorites`, { headers: authHeader() })
+);
 
 const getUserFavorites = userId => (
   axios.get(`${API_URL}users/${userId}/favorites`, { headers: authHeader() })
