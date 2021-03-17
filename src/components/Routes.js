eabc21ad9bc50756fdbde19cce40728318a902c2
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-17 12:50:46 +0000

    Create Routes component

---
+++ src/components/Routes.js
@@ -0,0 +1,102 @@
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+import history from '../helpers/history';
+import { logout } from '../redux/actions/auth';
+import clearMessage from '../redux/actions/message';
+
+import Login from './Login';
+import Register from './Register';
+import Home from './Home';
+import Profile from './Profile';
+import BoardUser from './BoardUser';
+
+const Routes = () => {
+  const { user: currentUser } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    history.listen(() => {
+      dispatch(clearMessage()); // clear message when changing location
+    });
+  }, [dispatch]);
+
+  const logOut = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <BrowserRouter history={history}>
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <Link to="/" className="navbar-brand">
+            NavBar Brand
+          </Link>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to="/home" className="nav-link">
+                Home
+              </Link>
+            </li>
+
+            {currentUser && (
+              <li className="nav-item">
+                <Link to="/user" className="nav-link">
+                  User
+                </Link>
+              </li>
+            )}
+          </div>
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/profile" className="nav-link">
+                  {currentUser.name}
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/logout" className="nav-link" onClick={logOut}>
+                  LogOut
+                </Link>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to="/login" className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to="/register" className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={['/', '/home', '/logout']} component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/profile" component={Profile} />
+            <Route path="/user" component={BoardUser} />
+          </Switch>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
