fe01a8ba3b8410a90f392b9d58dc24b4de94a9fd
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-17 10:55:13 +0000

    Remove the user key from reducers/message file

--- src/redux/reducers/message.js
+++ src/redux/reducers/message.js
@@ -2,7 +2,6 @@ import { SET_MESSAGE, CLEAR_MESSAGE } from '../actions/types';
 
 const initialstate = {
   message: '',
-  user: null,
 };
 
 const messageReducer = (state = initialstate, action) => {
