e5ebe9884133d610ece022a505be03994a3befd6
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-18 09:33:59 +0000

    Import and instantiate the Routes component in the App

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Routes from '../Routes';
+
 function App() {
   return (
     <div className="App">
-      Hello world
+      <Routes />
     </div>
   );
 }
