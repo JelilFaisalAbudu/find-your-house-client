a2d7456b8fe5e6b7b7efa3eb746dfcc5ac65f013
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-18 09:40:35 +0000

    Add the preloader houses and the favorites reducers to the combineReducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth';
 import messageReducer from './message';
+import housesReducer from './houses';
+import preloaderReducer from './preloader';
+import favoritesReducer from './favorites';
 
 const rootReducer = combineReducers({
+  loading: preloaderReducer,
+  houses: housesReducer,
+  favorites: favoritesReducer,
   auth: authReducer,
   message: messageReducer,
 });
