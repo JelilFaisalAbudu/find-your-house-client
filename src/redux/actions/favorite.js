b7f8219ef1543f65faf1a1150b94bc7653292d9d
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-19 07:50:49 +0000

    Encapsulate the favorite actions in async action creators

--- src/redux/actions/favorite.js
+++ src/redux/actions/favorite.js
@@ -1,18 +1,75 @@
-import { ADD_FAVORITE, REMOVE_FAVORITE } from './types';
+import { ADD_FAVORITE, REMOVE_FAVORITE, SET_MESSAGE } from './types';
+import userServices from '../../services/user.service';
 
-const addFavorite = house => ({
+const doAddFavorite = house => ({
   type: ADD_FAVORITE,
   payload: { house },
 });
 
-const removeFavorite = id => ({
+const doRemoveFavorite = houseId => ({
   type: REMOVE_FAVORITE,
-  payload: { id },
+  payload: { houseId },
 });
 
-const favorite = {
-  addFavorite,
-  removeFavorite,
+const addUserFavorite = (
+  userId,
+  houseId,
+) => dispatch => userServices.addUserFavorite(
+  userId,
+  houseId,
+).then(
+  data => {
+    dispatch(doAddFavorite(data));
+
+    return Promise.resolve();
+  },
+  error => {
+    const message = (error.response
+            && error.response.data
+            && error.response.data.message)
+          || error.message
+          || error.toString();
+
+    dispatch({
+      type: SET_MESSAGE,
+      payload: message,
+    });
+
+    return Promise.reject();
+  },
+);
+
+const removeUserFavorite = (
+  userId,
+  houseId,
+) => dispatch => userServices.removeUserFavorite(
+  userId,
+  houseId,
+).then(
+  data => {
+    dispatch(doRemoveFavorite(data));
+
+    return Promise.resolve();
+  },
+  error => {
+    const message = (error.response
+            && error.response.data
+            && error.response.data.message)
+          || error.message
+          || error.toString();
+
+    dispatch({
+      type: SET_MESSAGE,
+      payload: message,
+    });
+
+    return Promise.reject();
+  },
+);
+
+const userFavorites = {
+  addUserFavorite,
+  removeUserFavorite,
 };
 
-export default favorite;
+export default userFavorites;
