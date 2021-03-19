7c972477a7b98cd447dfcbe65292e779dcacf8dc
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-19 07:40:59 +0000

    Update the favorites reducer

--- src/redux/reducers/favorites.js
+++ src/redux/reducers/favorites.js
@@ -1,7 +1,25 @@
+import { ADD_FAVORITE, REMOVE_FAVORITE } from '../actions/types';
+
 const initialState = {
   favorites: [],
 };
 
-const favoritesReducer = (state = initialState) => state;
+const favoritesReducer = (state = initialState, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case ADD_FAVORITE:
+      return {
+        ...state,
+        favorites: [...state.favorites, payload.house],
+      };
+
+    case REMOVE_FAVORITE:
+      return state.filter(house => house.id !== payload.houseId);
+
+    default:
+      return state;
+  }
+};
 
 export default favoritesReducer;
