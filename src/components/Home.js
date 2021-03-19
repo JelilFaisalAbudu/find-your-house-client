c7a2c9639f57f5a09916003bc7dcf8922aed6216
Jelil Faisal Abudu <abudujelilfaisal@gmail.com>
2021-03-19 07:58:53 +0000

    Update the Home component with addUserFavorite action

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,20 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import userService from '../services/user.service';
+import houseImage from '../images/scott-webb-1ddol8rgUH8-unsplash.jpg';
+import userFavorites from '../redux/actions/favorite';
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector(state => state.auth);
   const [houses, setHouses] = useState([]);
   const [networkError, setError] = useState('');
+
+  const handleAddFavorite = (userId, houseId) => {
+    dispatch(userFavorites.addUserFavorite(userId, houseId));
+  };
 
   useEffect(() => {
     userService.getPublicContent().then(
@@ -34,19 +43,28 @@ const Home = () => {
     content = (
       houses.length
         ? (houses.map(house => (
-          <p key={`house-${house.id}`}>
-            <span>{house.name}</span>
-            {' '}
-            {' '}
-            <span>{house.description}</span>
-          </p>
+          <div key={house.id} className="card mb-3">
+            <img className="card-img-top" src={houseImage} alt={house.name} />
+            <div className="card-body">
+              <h5 className="card-title">{house.name}</h5>
+              <h5 className="card-title">{house.category}</h5>
+              <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+              <button
+                type="button"
+                className="card-text"
+                onClick={() => handleAddFavorite(user.id, house.id)}
+              >
+                <small className="text-muted">like</small>
+              </button>
+            </div>
+          </div>
         )))
         : (<div>Loading...</div>)
     );
   }
 
   return (
-    <div className="container">
+    <div className="">
       {content}
     </div>
   );
